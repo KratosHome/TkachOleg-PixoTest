7881a84fb30bb85c62f1468dac16c6febbe34596
olegtkach <olegontkach101@gmail.com>
2025-04-24 05:03:53 +0300

    feat: yarn, shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+          <Button>Click me</Button>
       <div>
-        <a href="https://vite.dev" target="_blank">
+
+
+      vc x   <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
